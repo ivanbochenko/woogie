388ba0885c87cee9628f479f07291aad3642e480
ivan <bochenkoivan@gmail.com>
2023-06-05 15:36:36 +0300

    u

--- app/(app)/Upgrade.tsx
+++ app/(app)/Upgrade.tsx
@@ -1,18 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import { Platform, Text, View, Alert } from 'react-native';
 
-import Purchases, { PurchasesOffering, PurchasesPackage, LOG_LEVEL } from 'react-native-purchases';
+import Purchases, { PurchasesOffering, PurchasesPackage, LOG_LEVEL, CustomerInfo } from 'react-native-purchases';
 import { APIKeys } from '../../constants/Keys';
 import { Pressable } from '../../components/Themed';
+import { useRouter } from 'expo-router';
+import { useAuth } from '../../lib/Auth';
 
 export default function Upgrade() {
+  const router = useRouter()
+  const { api, user  } = useAuth()
   const [currentOffering, setCurrentOffering] = useState<PurchasesOffering | null>(null);
-
-  // const customerInfo = await Purchases.getCustomerInfo();
+  const [info, setInfo] = useState<CustomerInfo>()
   
   useEffect(() => {
     const fetchData = async () => {
       const offerings = await Purchases.getOfferings();
+      const { customerInfo, created } = await Purchases.logIn(user?.id!);
+      if (created) {
+        setInfo(customerInfo)
+      }
       setCurrentOffering(offerings.current);
     };
 
@@ -33,7 +40,8 @@ export default function Upgrade() {
       const { productIdentifier, customerInfo } = await Purchases.purchasePackage(purchasePackage);
 
       if (typeof customerInfo.entitlements.active['pro'] !== 'undefined') {
-        // navigation.goBack();
+
+        router.back();
       }
     } catch (e: any) {
       if (!e.userCancelled) {
@@ -48,6 +56,7 @@ export default function Upgrade() {
     return (
       <View>
         <Text>Current Offering: {currentOffering.identifier}</Text>
+        <Text>Active Subscriptions{info?.activeSubscriptions}</Text>
         {
           currentOffering.availablePackages.map((pkg) => {
             const { product: { title, description, priceString } } = pkg;
