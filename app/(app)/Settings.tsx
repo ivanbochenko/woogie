bc230fe39fdb82f9188a5265b1741412dff0c229
ivan <bochenkoivan@gmail.com>
2023-06-10 19:05:27 +0300

    policy

--- app/(app)/Settings.tsx
+++ app/(app)/Settings.tsx
@@ -1,13 +1,14 @@
-import { SafeAreaView, ScrollView } from 'react-native';
+import { View, SafeAreaView, ScrollView, StyleSheet } from 'react-native';
 import { useTheme } from '@react-navigation/native';
 import Slider from '@react-native-community/slider';
 import { openURL } from 'expo-linking';
+import { useRouter, Link } from 'expo-router';
 
-import { View } from '../../components/Themed';
 import { Button } from '../../components/Button';
-import { BoldText } from '../../components/StyledText';
+import { BoldText, RegularText } from '../../components/StyledText';
 import { useAuth, signOut } from '../../lib/State'
-import { useRouter } from 'expo-router';
+import { ExternalLink } from '../../components/ExternalLink'
+import { m } from '../../constants/Spaces';
 
 export default function Settings() {
   const router = useRouter()
@@ -17,11 +18,7 @@ export default function Settings() {
 
   return (
     <SafeAreaView style={{flex: 1}}>
-      <ScrollView contentContainerStyle={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'space-evenly',
-      }}>
+      <ScrollView contentContainerStyle={styles.container}>
 
         <View>
           <BoldText>Max distance to event: {maxDistance} km</BoldText>
@@ -43,20 +40,35 @@ export default function Settings() {
           onPress={ () => router.push({pathname: 'Upgrade'}) }
         />
         <Button
-          title={'Feedback'}
-          onPress={() => openURL('mailto:woogie.ceo@gmail.com')}
-        />
-        <Button
-          title={'Agreement'}
-          onPress={ () => router.push({pathname: 'Agreement'}) }
-        />
-        <Button
           title={'Password'}
           onPress={ () => router.push({pathname: 'Password'}) }
         />
         <Button onPress={signOut} title={'Sign Out'}/>
 
+        <View style={styles.row}>
+          <Link href={'/Agreement'}>
+            <RegularText>Agreement</RegularText>
+          </Link>
+          <Link href={'/Policy'}>
+            <RegularText>Policy</RegularText>
+          </Link>
+          <RegularText onPress={() => openURL('mailto:woogie.ceo@gmail.com')}>Contact</RegularText>
+        </View>
+
       </ScrollView>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  row: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    gap: m
+  },
+});
