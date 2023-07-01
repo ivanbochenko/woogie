3c98e4c84af983f2c0b35d3f375a4efccf66afef
ivan <bochenkoivan@gmail.com>
2023-07-01 12:32:44 +0300

    m

--- lib/Client.ts
+++ lib/Client.ts
@@ -1,7 +1,7 @@
-import { createClient, cacheExchange, fetchExchange, subscriptionExchange } from 'urql'
+import { createClient, cacheExchange, fetchExchange } from 'urql'
 import axios from 'axios'
-import RNEventSource from 'react-native-event-source'
 import { API_URL } from '../constants/Config'
+import { yogaExchange } from '@graphql-yoga/urql-exchange'
 
 export const apiClient = axios.create({
   baseURL: API_URL,
@@ -14,48 +14,47 @@ export const apiClient = axios.create({
 
 export const gqlClient = (token: string) => createClient({
   url: API_URL + '/graphql',
-  fetchOptions: () => {
-    return {
-      headers: { Authorization: token }
-    };
-  },
+  fetchOptions: () => ({
+    headers: { Authorization: token }
+  }),
   exchanges: [
     cacheExchange,
     fetchExchange,
-    subscriptionExchange({
-      forwardSubscription(request) {
-        const url = new URL(API_URL + '/graphql')
-        url.searchParams.append('query', request.query || '')
-        if (request.variables) {
-          url.searchParams.append(
-            'variables',
-            JSON.stringify(request.variables),
-          )
-        }
-        return {
-          subscribe: (sink) => {
-            const eventsource = new RNEventSource(url.toString(), {
-              headers: { Authorization: token }
-            })
-            eventsource.addEventListener('message', (event) => {
-              const data = JSON.parse(event.data!)
-              sink.next(data)
-            })
-            eventsource.addEventListener('error', (error) => {
-              sink.error(error)
-            })
-            eventsource.addEventListener('complete', () => {
-              eventsource.close()
-            })
-            return {
-              unsubscribe: () => {
-                eventsource.removeAllListeners();
-                eventsource.close();
-              },
-            }
-          },
-        }
-      },
-    }),
+    yogaExchange(),
+    // subscriptionExchange({
+    //   forwardSubscription(request) {
+    //     const url = new URL(API_URL + '/graphql')
+    //     url.searchParams.append('query', request.query || '')
+    //     if (request.variables) {
+    //       url.searchParams.append(
+    //         'variables',
+    //         JSON.stringify(request.variables),
+    //       )
+    //     }
+    //     return {
+    //       subscribe: (sink) => {
+    //         const eventsource = new RNEventSource(url.toString(), {
+    //           headers: { Authorization: token }
+    //         })
+    //         eventsource.addEventListener('message', (event) => {
+    //           const data = JSON.parse(event.data!)
+    //           sink.next(data)
+    //         })
+    //         eventsource.addEventListener('error', (error) => {
+    //           sink.error(error)
+    //         })
+    //         eventsource.addEventListener('complete', () => {
+    //           eventsource.close()
+    //         })
+    //         return {
+    //           unsubscribe: () => {
+    //             eventsource.removeAllListeners();
+    //             eventsource.close();
+    //           },
+    //         }
+    //       },
+    //     }
+    //   },
+    // }),
   ],
 })
